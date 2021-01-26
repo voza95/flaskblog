e7fae1e7c6905dd1a519051bc05a1488e7a289b5
Vaibhav Oza <35027648+voza95@users.noreply.github.com>
2021-01-26 17:14:55 +0530

    Forms and User Input

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
-
+from flask import Flask, url_for, render_template, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '05afacefed2a03ae3bbe05258fc992c8'
 
 posts = [
     {
@@ -31,6 +33,25 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}.', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@gmail.com" and form.password.data == "password":
+            flash(f'You have logged in', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash(f'Login Unsuccessful', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 # set: for windows , export for linux
 # set FLASK_APP=flaskblog.py
